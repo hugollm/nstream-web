fddbf753991f883b7bd57571f650f69086faa4c6
Hugo Mota <hugo.txt@gmail.com>
2018-09-02 11:57:19 -0300

    Setup hello world with parcel and preact

---
+++ src/app/app.js
@@ -0,0 +1,15 @@
+import preact from 'preact';
+import './app.scss';
+
+
+export default class App extends preact.Component {
+
+    render() {
+        return <div id="app">
+            hello world
+        </div>;
+    }
+}
+
+
+preact.render(<App/>, document.body);
